5c941ebe6b7f5adb250c2bc06c306f3f2e1406d3
Caglar <Caglar@LPTRIT047.tr.watsons.com>
2023-02-14 12:25:07 +0300

    sc com

---
+++ Repository/NLayer.Repository/Contexts/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using System.Reflection;
+
+namespace NLayer.Repository.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature() { Id = 1, Color = "Kırmızı", Height = 100, Width = 200, ProductId = 1 },
+                new ProductFeature() { Id = 2, Color = "Mavi", Height = 100, Width = 400, ProductId = 2 });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Repository/NLayer.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using NLayer.Core.UnitOfWorks;
+using NLayer.Repository.Contexts;
+
+namespace NLayer.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
